0741ec60e56f06fef01fe205d4a52f5f337e38ad
Mateusz <mat646@gmail.com>
2019-05-08 12:25:36 +0200

    Example db content

---
+++ src/main/resources/database/example_content.sql
@@ -0,0 +1,6 @@
+insert into "user" values (12, 'John Smith', 'john@mail.com', 'password', 0);
+
+insert into "user" values (12, 'John Smith', 'john@mail.com', 'password', 0);
+
+insert into project (projectid, name, startdate, duration, closed)
+values ();
